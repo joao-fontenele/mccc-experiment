dc71b90ef679808d4de8a64fbb5606cd5a8e3505
João Paulo Fontenele Brito <jpfb@icomp.ufam.edu.br>
2017-07-15 22:16:52 -0400

    feat: implemented variation to the quicksort function

--- sort.c
+++ sort.c
@@ -23,18 +23,29 @@ int partition(int *array, int lo, int hi) {
   return i;
 }
 
-void _quicksort(int *array, int lo, int hi) {
-  int q = 0;
+int randomPartition(int *array, int lo, int hi) {
+  srand(time(NULL));
+  int random = (rand() % (hi - lo + 1)) + lo;
+  swap(&array[lo], &array[random]);
+  return partition(array, lo, hi);
+}
+
+void _quicksort(int *array, int lo, int hi, int (*partitionate) (int*, int, int)) {
+  int pivot = 0;
 
   if (hi > lo) {
-    q = partition(array, lo, hi);
-    _quicksort(array, lo, q - 1);
-    _quicksort(array, q + 1, hi);
+    pivot = partitionate(array, lo, hi);
+    _quicksort(array, lo, pivot - 1, partitionate);
+    _quicksort(array, pivot + 1, hi, partitionate);
   }
 }
 
 void quicksort(int *array, int n) {
-  _quicksort(array, 0, n - 1);
+  _quicksort(array, 0, n - 1, partition);
+}
+
+void quicksortRandom(int *array, int n) {
+  _quicksort(array, 0, n - 1, randomPartition);
 }
 
 void bubblesort(int *array, int n) {
--- sort.h
+++ sort.h
@@ -4,12 +4,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <time.h>
+#include "array.h"
 
 void swap(int *x, int *y);
 
 int partition(int *array, int lo, int hi);
-void _quicksort(int *array, int lo, int hi);
+int randomPartition(int *array, int lo, int hi);
+void _quicksort(int *array, int lo, int hi, int (*partitionate) (int*, int, int));
 void quicksort(int *array, int n);
+void quicksortRandom(int *array, int n);
 
 void bubblesort(int *array, int n);
 
